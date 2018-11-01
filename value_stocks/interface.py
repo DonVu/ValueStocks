fdb7bcf75b90bbe43181f610ca16e75b9db82425
Don Vu <don.vu1212@gmail.com>
2018-10-31 19:49:23 -0700

    Implement basic interface

--- value_stocks/interface.py
+++ value_stocks/interface.py
@@ -1,3 +1,8 @@
 def ask_stock_symbol():
-    # Stub for displaying interface
-    pass
+    # Display user prompt and ask for stock symbol
+    stock_symbol = input("Enter the stock ticker symbol for the company: ")
+    
+    return stock_symbol
+
+if __name__ == "__main__":
+    ask_stock_symbol()
