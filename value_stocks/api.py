9551df2ad92dff7cb34aaa54d2e0eaa7a3efb838
Don Vu <don.vu1212@gmail.com>
2018-11-07 18:57:28 -0800

    Add feature to convert json text into dictionary

--- value_stocks/api.py
+++ value_stocks/api.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def stocks_api(stock_symbol):
     # The api fetching stock data
@@ -8,7 +9,10 @@ def stocks_api(stock_symbol):
     
     if api_response.status_code == requests.codes.ok:
         # TODO(don): Remove the debugging print statement after testing function
-        print(api_response.text)
+        stock_text = api_response.text
+        edited_stock_text = stock_text.strip('<pre>')
+        example = json.loads(edited_stock_text)
+        #print(example['GOOG']['Revenue']['2013-12'])
 
 if __name__ == "__main__":
     stocks_api('GOOG')
