b5586173d3b2d08f9531981e279c98f465e72787
Don Vu <don.vu1212@gmail.com>
2018-11-26 19:54:36 -0800

    Connect api and interface to the main() method

--- value_stocks/stocks.py
+++ value_stocks/stocks.py
@@ -1,6 +1,12 @@
+from api import stocks_api
+from interface import ask_stock_symbol
+
 def main():
-    # Code stub for main()
-    pass
+    stock_ticker = ask_stock_symbol()
+
+    company_data = stocks_api(stock_ticker)
+
+    print(company_data.balance_sheet)
 
 if __name__ == "__main__":
     main()
