624467f3698d8c1885bfaeed454a2aa9d743dbbd
Don Vu <don.vu1212@gmail.com>
2018-11-26 19:41:10 -0800

    Change data fetched to placed into a StockData class

--- value_stocks/api.py
+++ value_stocks/api.py
@@ -1,6 +1,8 @@
 import requests
 import json
 
+from stock_data import StockData
+
 def stocks_api(stock_symbol):
     # The api fetching stock data
     url = 'https://financialmodelingprep.com/api/financials/'
@@ -10,8 +12,16 @@ def stocks_api(stock_symbol):
     api_balance_statement = requests.get(url + 'balance-sheet-statement/' +
         stock_symbol)
     api_cash_statement = requests.get(url + 'cash-flow-statement/' +
-        stock_symbol)  
-    
+        stock_symbol)
+
+    income_statement = response_processing(api_income_statement)
+    balance_statement = response_processing(api_balance_statement)
+    cash_statement = response_processing(api_cash_statement)
+
+    result = StockData(income_statement, balance_statement,
+            cash_statement)
+
+    return result
 
 def response_processing(api_response):
     if api_response.status_code == requests.codes.ok:
@@ -24,4 +34,8 @@ def response_processing(api_response):
         print('Error finding stock or api source')
 
 if __name__ == "__main__":
-    stocks_api('GOOG')
+    example_data = stocks_api('GOOG')
+
+    print(example_data.balance_sheet)
+    print(example_data.income_statement)
+    print(example_data.cash_flow_statement)
