abaab108bd2830ff81872a8397ccc91eb374f681
Don Vu <don.vu1212@gmail.com>
2018-12-09 23:31:12 -0800

    Add Working capital ratio

--- value_stocks/heuristic.py
+++ value_stocks/heuristic.py
@@ -1,4 +1,29 @@
-def ai_heuristic():
-    # Stub for the value investment heuristic
-    pass
+import json
+
+from api import stocks_api
+
+def ai_heuristic(company):
+    stock_rating = 0
+
+    """ Balance sheet ratios """
+
+    """ Working Capital """
+    if (company.balance_sheet[company.stock_name]['Total current assets']['2017-09'] >
+            company.balance_sheet[company.stock_name]['Total current liabilities']['2017-09']):
+        stock_rating += 10
+
+        print("Working capital is sufficient to pay short term debt(+10)")
+    else:
+        stock_rating -= 10
+        print("Working capital is negative. Not enough to pay short term debt(-10)")
+
+    return stock_rating       
+
+if __name__ == "__main__":
+    example_company = stocks_api("AAPL")
+
+    print(ai_heuristic(example_company))
+
+
+
 
