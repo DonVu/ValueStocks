3a697a81f45bc9dbcddf15a19e285db6874342d0
Don Vu <don.vu1212@gmail.com>
2018-12-10 18:59:58 -0800

    Fix return on equity bug

--- value_stocks/heuristic.py
+++ value_stocks/heuristic.py
@@ -8,8 +8,8 @@ def ai_heuristic(company):
     """ Balance sheet ratios """
 
     """ Working Capital """
-    if (company.balance_sheet[company.stock_name]['Total current assets']['2017-09'] >
-            company.balance_sheet[company.stock_name]['Total current liabilities']['2017-09']):
+    if (int(company.balance_sheet[company.stock_name]['Total current assets']['2017-09']) >
+            int(company.balance_sheet[company.stock_name]['Total current liabilities']['2017-09'])):
         stock_rating += 10
 
         print("Working capital is sufficient to pay short term debt(+10)")
@@ -19,12 +19,12 @@ def ai_heuristic(company):
 
 
     """ Return On Equity ratio """
-    return_on_equity = (int(company.income_statement[company.stock_name]['Net income']['2017-09']) /
-        int(company.balance_sheet[company.stock_name]["Total stockholders' equity"]['2017-09']))
+    return_on_equity = (float(company.income_statement[company.stock_name]['Net income']['2017-09']) /
+        float(company.balance_sheet[company.stock_name]["Total stockholders' equity"]['2017-09']))
     if (return_on_equity > 0.10):
         stock_rating += 30
 
-        print("Return on equity is healthy at: %.2f (+30)", return_on_equity)
+        print("Return on equity is healthy at: %.2f (+30)" %(return_on_equity))
     else:
         stock_rating -= 20
         print("Return on equity is below average(-20)")
